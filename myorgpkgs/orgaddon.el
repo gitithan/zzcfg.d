9d34ca30a477d22110f6f5531362ecdc38022fcf
gitithan <gitit.han@gmail.com>
2012-09-07 17:55:59 +0800

    touch lng-font and orgaddon

--- myorgpkgs/orgaddon.el
+++ myorgpkgs/orgaddon.el
@@ -608,4 +608,20 @@
 
 
 
+;; Make TAB the yas trigger key in the org-mode-hook and enable flyspell mode and autofill
+;;============================
+;;(add-hook 'org-mode-hook
+;;          (lambda ()
+;;            ;; yasnippet
+;;            (make-variable-buffer-local 'yas/trigger-key)	;;'yas/expand
+;;            (org-set-local 'yas/trigger-key [tab])
+;;            (define-key yas/keymap [tab] 'yas/next-field-group)
+;;            ;; flyspell mode for spell checking everywhere
+;;            ;;(flyspell-mode 1)
+;;            ;; auto-fill mode on
+;;            (auto-fill-mode 1)))
+
+
+
+
 (provide 'orgaddon)
